be8abeed5101c9e84a9f40b2af9e62bbc00dada6
Marco Avila <marcotulio.avila@gmail.com>
2024-07-18 16:05:33 -0700

    chore: change playwright reports for ci to html and github

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,6 +7,7 @@ const config: PlaywrightTestConfig = {
     },
     testDir: 'tests',
     testMatch: /(.+\.)?(test|spec)\.[jt]s/,
+    reporter: process.env.CI ? [['github'], ['html', { open: 'never' }]] : 'list',
 };
 
 export default config;
