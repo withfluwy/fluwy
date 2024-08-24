0f3d46c2ab7dc14edc6e9a404a0e4b34946ac094
Marco Avila <marcotulio.avila@gmail.com>
2024-08-23 23:54:35 -0700

    new link component

--- src/lib/components/primitives/index.ts
+++ src/lib/components/primitives/index.ts
@@ -9,3 +9,4 @@ export { default as H5 } from './h5.svelte';
 export { default as H6 } from './h6.svelte';
 export { default as P } from './p.svelte';
 export { default as Image } from './image.svelte';
+export { default as Link } from './link.svelte';
