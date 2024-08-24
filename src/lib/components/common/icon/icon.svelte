70498a8bf2ce8d2f25407f4ae345db0602479b74
Marco Avila <marcotulio.avila@gmail.com>
2024-08-24 16:52:13 -0700

    starting documentation barebones

--- src/lib/components/common/icon/icon.svelte
+++ src/lib/components/common/icon/icon.svelte
@@ -2,7 +2,7 @@
     import { useTheme } from '@/lib/core/client/index.js';
     import type { IconProps } from './types.js';
 
-    const defaultIconSize = useTheme('common.defaults.icon_size', 20);
+    const defaultIconSize = useTheme('common.icon_size', 20);
     const { name, size = defaultIconSize, content, ...props }: IconProps = $props();
     const icon = $derived(name ?? content);
 </script>
