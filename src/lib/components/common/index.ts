1ab971e5442a46d84912271b7219e3fa7d40c71f
Marco Avila <marcotulio.avila@gmail.com>
2024-08-24 15:04:48 -0700

    new repeat component

--- src/lib/components/common/index.ts
+++ src/lib/components/common/index.ts
@@ -3,3 +3,4 @@ export { default as Dropdown } from './dropdown/dropdown.svelte';
 export { default as DropdownItem } from './dropdown/dropdown-item.svelte';
 export { default as DropdownLabel } from './dropdown/dropdown-label.svelte';
 export { default as DropdownSeparator } from './dropdown/dropdown-separator.svelte';
+export { default as Repeat } from './repeat/repeat.svelte';
---
+++ src/lib/components/common/repeat/repeat.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import type { Any } from '@/lib/core/contracts.js';
+    import { Render } from '@/lib/core/index.js';
+    import { exclude } from '@/lib/core/utils/index.js';
+
+    interface RepeatProps {
+        times: number;
+        content?: Any;
+    }
+
+    const props: RepeatProps = $props();
+</script>
+
+{#each Array(props.times) as _, i}
+    <Render props={exclude(props, 'times')} />
+{/each}
