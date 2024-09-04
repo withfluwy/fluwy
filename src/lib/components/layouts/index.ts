e1010a9fbad2d57db79f4588fbbd85b773887c36
Marco Avila <marcotulio.avila@gmail.com>
2024-09-04 16:45:37 -0700

    New spacer component

--- src/lib/components/layouts/index.ts
+++ src/lib/components/layouts/index.ts
@@ -1,3 +1,4 @@
 export { default as Screen } from './screen.svelte';
 export { default as DarkModeToggler } from './dark-mode-toggler.svelte';
 export { default as Brand } from './brand.svelte';
+export { default as Spacer } from './spacer.svelte';
