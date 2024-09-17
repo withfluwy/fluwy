aab7a4ca990b8249bb161a1a0d3c50b19e20a0af
Marco Avila <marcotulio.avila@gmail.com>
2024-09-17 14:47:56 -0700

    fix(tests): update screenshots for home page.

--- tests/home.test.ts
+++ tests/home.test.ts
@@ -1,6 +1,12 @@
 import { expect, test } from '@playwright/test';
 
-test('has screenshot', async ({ page }) => {
+test('light mode', async ({ page }) => {
+    await page.goto('/');
+    await expect(page).toHaveScreenshot();
+});
+
+test('dark mode', async ({ page }) => {
+    await page.emulateMedia({ colorScheme: 'dark' });
     await page.goto('/');
     await expect(page).toHaveScreenshot();
 });
