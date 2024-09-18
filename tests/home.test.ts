07fb791acc77dad1f63de63410b27dd8ff46758f
Marco Avila <marcotulio.avila@gmail.com>
2024-09-18 05:35:38 -0700

    fix(tests): and upgrade packages

--- tests/home.test.ts
+++ tests/home.test.ts
@@ -2,11 +2,11 @@ import { expect, test } from '@playwright/test';
 
 test('light mode', async ({ page }) => {
     await page.goto('/');
-    await expect(page).toHaveScreenshot();
+    await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.02 });
 });
 
 test('dark mode', async ({ page }) => {
     await page.emulateMedia({ colorScheme: 'dark' });
     await page.goto('/');
-    await expect(page).toHaveScreenshot();
+    await expect(page).toHaveScreenshot({ maxDiffPixelRatio: 0.02 });
 });
