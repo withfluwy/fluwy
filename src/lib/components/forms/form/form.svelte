382a87d54b67e1968e20640096e8e9702e851be5
Marco Avila <marcotulio.avila@gmail.com>
2024-12-10 19:11:01 -0800

    feat(forms): new form component

---
+++ src/lib/components/forms/form/form.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import { cn } from '@/lib/core/utils/index.js';
+    import type { FormProps, FormState } from './types.js';
+    import { Render, useClient, createContext } from '@/lib/core/index.js';
+    import { collapseObject } from '@/lib/core/utils/normalize-object/index.js';
+    import { setupContext } from '@/lib/core/context/index.js';
+
+    const { id, content, ...props }: FormProps = $props();
+
+    const client = useClient();
+    const context = createContext();
+
+    /**
+     * Forms should create its own context for its data and validation errors. This context will be accessible to all
+     * child components.
+     */
+    setupContext(context);
+
+    const form: FormState = $state({
+        data: collapseObject(props.data ?? {}),
+        errors: {},
+    });
+
+    const method = $derived((props.method || 'POST') as 'POST' | 'GET' | 'DIALOG');
+
+    context.set('form', form);
+
+    async function onsubmit(event: SubmitEvent) {
+        if (!props.on_submit) return;
+        event.preventDefault();
+
+        await client.handleOperations(props.on_submit, context);
+    }
+</script>
+
+<form {id} {onsubmit} {method} class={cn('flex flex-col gap-3', props.class)} enctype="multipart/form-data">
+    <Render props={content} />
+</form>
