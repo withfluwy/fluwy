2dade5e1d1e5782d2c92da45848799246ad64fec
Marco Avila <marcotulio.avila@gmail.com>
2024-12-13 07:56:02 -0800

    feat(theme): adds `layout.column` and `layout.row` themes
    Also sets the default gap for rows and columns of `gap-4`.

--- src/lib/components/primitives/row.svelte
+++ src/lib/components/primitives/row.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+    import { useTheme } from '@/lib/core/client/index.js';
     import { Render, type ElementProps } from '@/lib/core/index.js';
     import { cn } from '@/lib/core/utils/index.js';
 
     const props: ElementProps = $props();
+    const rowTheme = useTheme('layout.row');
 </script>
 
-<div class={cn('flex w-full', props.class)}>
+<div class={cn('flex w-full gap-4', rowTheme, props.class)}>
     <Render {props} />
 </div>
