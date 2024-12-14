1e776cdac24876261d2e6868865d02153b46256a
Marco Avila <marcotulio.avila@gmail.com>
2024-12-13 17:28:21 -0800

    chore(tests): add tests for nested data and reactivity

--- tests/components/forms/contacts-form.spec.ts
+++ tests/components/forms/contacts-form.spec.ts
@@ -20,33 +20,33 @@ test('create contacts with button.on_click event', async ({ page }) => {
     await page.getByText('Please enter a valid email').click();
 
     await expect(page.locator('#main')).toMatchAriaSnapshot(`
-          - heading "Contact Form - Button Submit" [level=5]
-          - text: First Name
-          - textbox "First Name"
-          - text: This field is required. Last Name
-          - textbox "Last Name"
-          - text: This field is required. Email Address
-          - textbox "Email Address"
-          - text: Please enter a valid email address.
-          - button "Clear errors"
-          - button "Create Contact":
-            - img
-          `);
+        - heading "Contact Form - Button Submit" [level=5]
+        - text: First Name
+        - textbox "First Name"
+        - text: This field is required. Last Name
+        - textbox "Last Name"
+        - text: This field is required. Email Address
+        - textbox "Email Address"
+        - text: Please enter a valid email address.
+        - button "Clear errors"
+        - button "Create Contact":
+          - img
+        `);
 
     await page.getByRole('button', { name: 'Clear errors' }).nth(0).click();
 
     await expect(page.locator('#main')).toMatchAriaSnapshot(`
-            - heading "Contact Form - Button Submit" [level=5]
-            - text: First Name
-            - textbox "First Name"
-            - text: Last Name
-            - textbox "Last Name"
-            - text: Email Address
-            - textbox "Email Address"
-            - button "Clear errors"
-            - button "Create Contact":
-              - img
-            `);
+        - heading "Contact Form - Button Submit" [level=5]
+        - text: First Name
+        - textbox "First Name"
+        - text: Last Name
+        - textbox "Last Name"
+        - text: Email Address
+        - textbox "Email Address"
+        - button "Clear errors"
+        - button "Create Contact":
+          - img
+    `);
 });
 
 test('create contact with button.on_click on slow connection', async ({ page }) => {
@@ -72,18 +72,18 @@ test('create contact with button.on_click on slow connection', async ({ page }) 
     await expect(loader).toBeVisible();
 
     await expect(page.locator('#main')).toMatchAriaSnapshot(`
-      - heading "Contact Form - Button Submit" [level=5]
-      - text: First Name
-      - textbox "First Name"
-      - text: This field is required. Last Name
-      - textbox "Last Name"
-      - text: This field is required. Email Address
-      - textbox "Email Address"
-      - text: Please enter a valid email address.
-      - button "Clear errors"
-      - button "Create Contact":
-        - img
-      `);
+        - heading "Contact Form - Button Submit" [level=5]
+        - text: First Name
+        - textbox "First Name"
+        - text: This field is required. Last Name
+        - textbox "Last Name"
+        - text: This field is required. Email Address
+        - textbox "Email Address"
+        - text: Please enter a valid email address.
+        - button "Clear errors"
+        - button "Create Contact":
+          - img
+    `);
 
     await expect(loader).not.toBeVisible();
 });
@@ -118,22 +118,22 @@ test('create contact from on_submit on forms', async ({ page }) => {
         - button "Clear errors"
         - button "Create Contact":
           - img
-        `);
+    `);
 
     await page.getByRole('button', { name: 'Clear errors' }).nth(1).click();
 
     await expect(page.locator('#main')).toMatchAriaSnapshot(`
-          - heading "Contact Form - Form Submit" [level=5]
-          - text: First Name
-          - textbox "First Name"
-          - text: Last Name
-          - textbox "Last Name"
-          - text: Email Address
-          - textbox "Email Address"
-          - button "Clear errors"
-          - button "Create Contact":
-            - img
-          `);
+        - heading "Contact Form - Form Submit" [level=5]
+        - text: First Name
+        - textbox "First Name"
+        - text: Last Name
+        - textbox "Last Name"
+        - text: Email Address
+        - textbox "Email Address"
+        - button "Clear errors"
+        - button "Create Contact":
+          - img
+    `);
 });
 
 test('create contact from on_submit on forms on slow connection', async ({ page }) => {
@@ -159,18 +159,77 @@ test('create contact from on_submit on forms on slow connection', async ({ page 
     await expect(loader).toBeVisible();
 
     await expect(page.locator('#main')).toMatchAriaSnapshot(`
-    - heading "Contact Form - Form Submit" [level=5]
-    - text: First Name
-    - textbox "First Name"
-    - text: This field is required. Last Name
-    - textbox "Last Name"
-    - text: This field is required. Email Address
-    - textbox "Email Address"
-    - text: Please enter a valid email address.
-    - button "Clear errors"
-    - button "Create Contact":
-      - img
+        - heading "Contact Form - Form Submit" [level=5]
+        - text: First Name
+        - textbox "First Name"
+        - text: This field is required. Last Name
+        - textbox "Last Name"
+        - text: This field is required. Email Address
+        - textbox "Email Address"
+        - text: Please enter a valid email address.
+        - button "Clear errors"
+        - button "Create Contact":
+            - img
     `);
 
     await expect(loader).not.toBeVisible();
+});
+
+test('contacts form with nested data', async ({ page }) => {
+    await page.goto('/components/forms/tests');
+
+    await test.step('check form initial data', async () => {
+        await expect(page.locator('#main')).toMatchAriaSnapshot(`
+            - heading "Contact Form - Nested Data" [level=5]
+            - textbox "Email Address"
+            - text: Address
+            - textbox "Address": /\\d+ Main St/
+            - textbox "City": London
+            - textbox "Country"
+            - button "Submit":
+                - img
+            - text: "/\\\\{ \\"firstName\\": \\"John\\", \\"lastName\\": \\"Doe\\", \\"email\\": \\"john\\\\.doe\\", \\"address\\": \\\\{ \\"line1\\": \\"\\\\d+ Main St\\", \\"city\\": \\"London\\", \\"country\\": \\"UK\\" \\\\} \\\\}/"
+        `);
+    });
+
+    await test.step('change email and address', async () => {
+        await page.getByPlaceholder('Email Address').click();
+        await page.getByPlaceholder('Email Address').fill('john.doe@mail.com');
+        await page.getByPlaceholder('Address Line').click();
+        await page.getByPlaceholder('Address Line').press('ControlOrMeta+a');
+        await page.getByPlaceholder('Address Line').fill('255 156 St');
+        await page.getByPlaceholder('Country').dblclick();
+        await page.getByPlaceholder('Country').fill('CA');
+
+        await expect(page.locator('#main')).toMatchAriaSnapshot(`
+            - heading "Contact Form - Nested Data" [level=5]
+            - textbox "Email Address": john.doe@mail.com
+            - text: Address
+            - textbox "Address": /\\d+ \\d+ St/
+            - textbox "City": London
+            - textbox "Country"
+            - button "Submit":
+                - img
+            - text: "/\\\\{ \\"firstName\\": \\"John\\", \\"lastName\\": \\"Doe\\", \\"email\\": \\"john\\\\.doe@mail\\\\.com\\", \\"address\\": \\\\{ \\"line1\\": \\"\\\\d+ \\\\d+ St\\", \\"city\\": \\"London\\", \\"country\\": \\"CA\\" \\\\} \\\\}/"
+        `);
+    });
+
+    await test.step('check validation errors', async () => {
+        await page.getByRole('button', { name: 'Submit' }).click();
+
+        await expect(page.locator('#main')).toMatchAriaSnapshot(`
+            - heading "Contact Form - Nested Data" [level=5]
+            - textbox "Email Address": john.doe@mail.com
+            - text: Email is required Address
+            - textbox "Address": /\\d+ \\d+ St/
+            - text: Address line 1 is required
+            - textbox "City": London
+            - text: City is required
+            - textbox "Country"
+            - text: Country is required
+            - button "Submit":
+                - img
+            - text: "/\\\\{ \\"firstName\\": \\"John\\", \\"lastName\\": \\"Doe\\", \\"email\\": \\"john\\\\.doe@mail\\\\.com\\", \\"address\\": \\\\{ \\"line1\\": \\"\\\\d+ \\\\d+ St\\", \\"city\\": \\"London\\", \\"country\\": \\"CA\\" \\\\} \\\\}/"
+        `);
+    });
 });
